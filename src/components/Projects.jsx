ea87785f61eaf77ef609259c33b3f137be580870
Andreas <andrerim@stud.ntnu.no>
2020-02-15 23:26:17 +0100

    Removed dead code

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import "./projects.css";
 
 const Project = props => {
-  return (
-    <div className="project">
-      <a href={props.projectLink}><img className="projectimg" src={props.projectimg}  width="400px" alt="project screenshot"></img></a>
-      <div className="projectinfo">
-        <h1>
-          <a className="demoLink"href={props.projectLink}>{props.projectName}</a>
-        </h1>
-        <p>
-          {props.projectSummary + " "} 
-          <a id="repLink" href={props.projectRep}>
-            Repository
+    return (
+        <div className="project">
+            <a href={props.projectLink}><img className="projectimg" src={props.projectimg} width="400px" alt="project screenshot"></img></a>
+            <div className="projectinfo">
+                <h1>
+                    <a href={props.projectLink}>{props.projectName}</a>
+                </h1>
+                <p>
+                    {props.projectSummary + " "}
+                    <a id="repLink" href={props.projectRep}>
+                        Repository
           </a>
-        </p>
-      </div>
-    </div>
-  );
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default Project;
